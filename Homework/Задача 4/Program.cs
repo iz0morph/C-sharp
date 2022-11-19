40631eb5fe6ff4b49b3a9eb88db8a3705c89f32a
Dmitriy Boytsov <hero575@mail.ru>
2022-11-19 21:01:48 +0300

    task 4

---
+++ Homework/Задача 4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число - ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (a>b)
+{
+    max=a;
+}
+    else
+    {
+         max=b;
+    }
+   
+if (c>max) max=c;
+Console.WriteLine($"Максимальное число - {max}");
