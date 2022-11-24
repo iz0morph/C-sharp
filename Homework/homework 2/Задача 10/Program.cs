1a7ab25b892fabc3374e3fbb92eb9befa725bbe5
Dmitriy Boytsov <hero575@mail.ru>
2022-11-24 21:17:16 +0300

    task 1

---
+++ Homework/homework 2/Задача 10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<1000 & a>99) //Проверка, что введенное число удовлетворяет условию задачи
+{
+    int num1 = (a/10)%10;
+    Console.WriteLine(num1);
+}
+else Console.WriteLine("Введенное число не трехзначное");
