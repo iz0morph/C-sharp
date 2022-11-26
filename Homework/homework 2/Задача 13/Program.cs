09d3fcbe94b7c64171aa1c8c4eed19ba85b1eabe
Dmitriy Boytsov <hero575@mail.ru>
2022-11-26 20:48:23 +0300

    task 2

---
+++ Homework/homework 2/Задача 13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите любое число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>99)
+{
+    while (a>999)
+    {
+        a = a/10;
+    }
+    a = (a/10)%10;
+    Console.WriteLine(a);
+}
+else Console.WriteLine("Третьей цифры нет");
