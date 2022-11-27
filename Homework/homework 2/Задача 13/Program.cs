5229bfbb8898134987411229f891a3cd976d479e
Dmitriy Boytsov <hero575@mail.ru>
2022-11-27 18:26:40 +0300

    task 2

--- Homework/homework 2/Задача 13/Program.cs
+++ Homework/homework 2/Задача 13/Program.cs
@@ -1,12 +1,13 @@
-﻿Console.Write("Введите любое число - ");
+﻿Console.Write("Введите любое целое число - ");
 int a = Convert.ToInt32(Console.ReadLine());
-if (a>99)
+int b =Math.Abs(a);
+if (b>99)
 {
-    while (a>999)
+    while (b>999)
     {
-        a = a/10;
+        b = b/10;
     }
-    int num = a%10;
+    int num = b%10;
     Console.WriteLine($"Третья цифра - {num}");
 }
 else Console.WriteLine("Третьей цифры нет");
