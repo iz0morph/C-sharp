4612f93260b79335436a415030af1012232cd6f9
Dmitriy Boytsov <hero575@mail.ru>
2022-11-27 18:18:58 +0300

    task 3

---
+++ Homework/homework 2/Задача 15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите порядковый номер дня недели - ");
+int a = Convert.ToInt32 (Console.ReadLine());
+if (a==6 || a==7)
+{
+    Console.WriteLine("Данный день - выходной");
+}
+else
+{
+    if (a<6 && a>0) Console.WriteLine("Данный день НЕ выходной");
+    else Console.WriteLine("Неверный номер дня недели");
+}
