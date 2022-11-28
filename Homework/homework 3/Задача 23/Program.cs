82caf2f37a3fc2f3af9b4862eecefbe3f0cfd4bc
Dmitriy Boytsov <hero575@mail.ru>
2022-11-28 21:18:49 +0300

    task 23

---
+++ Homework/homework 3/Задача 23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a<2)
+{
+    Console.Write("Введите ПОЛОЖИТЕЛЬНОЕ число >1 - ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+for (int b=1; b<=a; b++)
+    {
+        Console.WriteLine($"Куб числа {b} = {Math.Pow(b,3)}");
+    }
