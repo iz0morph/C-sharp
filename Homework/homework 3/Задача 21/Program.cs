d9fe78b363c6a5aaee819324ca356fd0a40f9303
Dmitriy Boytsov <hero575@mail.ru>
2022-11-28 21:09:10 +0300

    task 21

---
+++ Homework/homework 3/Задача 21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координату x первой точки:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y первой точки:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z первой точки:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату x второй точки:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y второй точки:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z второй точки:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result1 = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+double result2=Math.Round (result1, 3);
+Console.WriteLine($"Расстояние между точками = {result2}");
