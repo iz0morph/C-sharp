55f8c99fcb09156687deba759b60adb5e382db94
Dmitriy Boytsov <hero575@mail.ru>
2022-12-09 20:48:30 +0300

    Задача 27

---
+++ Homework/homework 4/Задача 27/Program.cs
@@ -0,0 +1,16 @@
+﻿void sum(int a)
+{
+    int b = 0;
+    int c = 0;
+    while (a != 0)
+    {
+        c = a % 10;
+        b = b + c;
+        a = a / 10;
+    }
+    Console.WriteLine(b);
+
+}
+Console.WriteLine("Введите число - ");
+int x = Convert.ToInt32(Console.ReadLine());
+sum(x);
