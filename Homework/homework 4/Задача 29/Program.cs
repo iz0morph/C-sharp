231e754ddbacfcffe1cfb3b81aad824e2c8ce34b
Dmitriy Boytsov <hero575@mail.ru>
2022-12-09 21:04:39 +0300

    Задача 29

---
+++ Homework/homework 4/Задача 29/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection, int n)
+{
+    int index=0;
+    while (index<n)
+    {
+        collection [index]=new Random().Next();
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col, int n)
+{
+    int count=n;
+    int position=0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+Console.Write("Введите кол-во элементов массива - ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[x];
+FillArray(array, x);
+PrintArray(array, x);
