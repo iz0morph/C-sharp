4e4d6e2232ee5e0e645b2481c900293707b9e538
Dmitriy Boytsov <hero575@mail.ru>
2022-12-09 20:49:51 +0300

    update

---
+++ Homework/homework 1/Задача 6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+    Console.WriteLine("Введенное число - четное");
+}
+else{
+    Console.WriteLine("Введенное число - нечетное");
+}
