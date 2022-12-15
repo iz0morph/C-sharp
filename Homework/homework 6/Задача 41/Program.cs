e871ded8a6d83eb286390f4160c0f7e027ed2302
Dmitriy Boytsov <hero575@mail.ru>
2022-12-15 14:17:55 +0300

    done

---
+++ Homework/homework 6/Задача 41/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите числа - ");
+string x = Console.ReadLine();
+string[] nums = x.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+int[] array = nums.Select(Int32.Parse).ToArray();
+
+void Count(int[] col)
+{
+    int counter = 0;
+    for (int i = 0; i < col.Length; i++)
+    {
+        if (col[i] > 0) counter++;
+    }
+    Console.WriteLine($"Количество чисел больше 0 - {counter}");
+}
+
+Count(array);
---
+++ Homework/homework 6/Задача 43/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите значение k1 - ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b1 - ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2 - ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2 - ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = { k1, b1, k2, b2 };
+
+void intersectPoint(int[] n)
+{
+    double mainDeterm = -k1 + k2;
+    double det1 = -(-b1 + b2);
+    double det2 = -(k1 * b2 - k2 * b1);
+    if (mainDeterm == 0 && det1 == 0 && det2 == 0) Console.WriteLine("Прямые совпадают");
+    else if (mainDeterm == 0) Console.WriteLine("Прямые параллельны");
+    else
+    {
+        double x = det1 / mainDeterm;
+        double y = det2 / mainDeterm;
+        Console.WriteLine($"Координаты точки пересечения: ({x};{y})");
+    }
+}
+
+intersectPoint(arr);
