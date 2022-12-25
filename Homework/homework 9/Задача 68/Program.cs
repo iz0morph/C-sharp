794a464b7045a359f963f5b5a4c2525049e20d13
Dmitriy Boytsov <hero575@mail.ru>
2022-12-25 18:14:28 +0300

    done

--- Homework/homework 9/Задача 68/Program.cs
+++ Homework/homework 9/Задача 68/Program.cs
@@ -1,6 +1,5 @@
 ﻿int Akkerman(int m, int n)
 {
-
     if (m == 0) return n + 1;
     else if (m > 0 && n == 0)
     {
@@ -10,11 +9,15 @@
     {
         return Akkerman(m - 1, Akkerman(m, n - 1));
     }
-    else return 0;
+    else
+    {
+        Console.WriteLine("Одно или два из введенных значений отрицательны");
+        return 0;
+    }
 }
-
 Console.WriteLine("Введите первое число: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Результат вычисления функции Аккермана А({m},{n}) = ");
 Console.WriteLine(Akkerman(m, n));
